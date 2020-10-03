1f9867cdef18c22c54d778a20e88408d566f8d6e
wglee0511 <69962377+wglee0511@users.noreply.github.com>
2020-10-03 18:15:15 +0900

    update User page (1/3) make user page to xml file set user page code

--- app/src/main/java/org/techtown/lineup/MainActivity.kt
+++ app/src/main/java/org/techtown/lineup/MainActivity.kt
@@ -8,6 +8,7 @@ import android.view.MenuItem
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import org.techtown.lineup.navigation.*
 
@@ -40,10 +41,15 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
             }
             R.id.action_account -> {
                 var userFragment = UserFragment()
+                var bundle = Bundle()
+                var uid = FirebaseAuth.getInstance().currentUser?.uid
+                bundle.putString("destinationUid",uid)
+                userFragment.arguments = bundle
                 supportFragmentManager.beginTransaction()
                     .replace(R.id.main_content, userFragment).commit()
                 return true
             }
+
         }
         return false
     }
